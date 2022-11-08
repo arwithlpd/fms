5026a060374903c811484c6bd9d297012ad814a1
Abdur Rahman <arwithlpd@gmail.com>
2022-11-08 15:57:12 +0600

    font delete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css'
+import { Typography } from '@mui/material'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, laboriosam!</header>
+      <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, facere!</Typography>
+      <h2>
+        <i className='fab fa-4x fa-js'> </i>
+        <i className='fab fa-4x fa-react'> </i>
+        <i className='fab fa-4x fa-angular'> </i>
+        <i className='fab fa-4x fa-vuejs'> </i>
+        <i className='fab fa-4x fa-node'> </i>
+      </h2>
     </div>
   )
 }
