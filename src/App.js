b6ae86c4e1e51476c84281dd92dfbfd0785f4597
Abdur Rahman <arwithlpd@gmail.com>
2022-11-08 16:26:27 +0600

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import MenuIcon from '@mui/icons-material/Menu'
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import React from 'react'
 
-function App() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant='h6'
-            component='div'
-            sx={{ flexGrow: 1 }}
-          >
-            News
-          </Typography>
-          <Button color='inherit'>Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  )
+const App = () => {
+  return <div>Hello world</div>
 }
 
 export default App
